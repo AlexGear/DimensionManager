129522647d366432470f1ca5ab55d6e6a2d0a246
Daria Malygina <dmalygina@rit-it.com>
2019-02-15 12:24:42 +0700

    Rename Form1 to MainForm

--- DimensionManager/MainForm.Designer.cs
+++ DimensionManager/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DimensionManager
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
--- DimensionManager/MainForm.cs
+++ DimensionManager/MainForm.cs
@@ -10,13 +10,14 @@ using System.Windows.Forms;
 
 namespace DimensionManager
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
             axisSelectCombo.SelectedIndex = 0;
 
         }
+    }
     }
 }
